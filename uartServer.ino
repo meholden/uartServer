32d38b42bc939d17fba44e16ef1e9d4723664d42
meholden <mholden>
2016-12-29 14:50:33 -0800

    nice version, ring buffer, 1 second update rate

--- uartServer.ino
+++ uartServer.ino
@@ -1,3 +1,13 @@
+// This code runs a webserver on the ESP8266 chip
+// data that comes in on the serial port is buffered
+// The root webpage displays the serial data back line-by-line
+// The web page will refresh itself
+// Not a very efficient serial-wifi bridge 
+//  but the html is a good jumping off point for other things.
+// Modified from ESP8266 library code
+// Mike Holden 11/2016
+
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -8,9 +18,11 @@
 const char* ssid = MYSSID; // #defined in mikepw.h for github security
 const char* password = MYWIFIPW; // easier to just do ssid = "theapyouwant" but not good for github security
 
-#define MYBUFSIZE 600
+#define MYBUFSIZE 1200
+#define HEADFOOTSIZE 100
 char serbuf[MYBUFSIZE]="Hi from esp8266!\r\n";
-int bi=0,bo=0;
+char htmlbuf[MYBUFSIZE+HEADFOOTSIZE];
+int bi=0,bo=0,hlen,rbfirst,rblast,ii,jj;
 
 ESP8266WebServer server(80);
 
@@ -25,9 +37,28 @@ void handleRoot() {
 }
 
 void handleRoot2() {
-  server.send(200, "text/html", serbuf);
-  String lala="this is the bitter end";
-  server.sendContent(lala);
+  // have to insert the html header, then unwrap the ring buffer, then add the footer
+  // saved 100 bytes for the header-footer characters so keep track!
+  // hlen is how long the header is (header initialized in setup)
+  // buffer between hlen and 99 should be blank (footer space, used to copy too)
+  jj=hlen; // empty byte after header
+  bi=rbfirst;
+  while (bi != rblast) {
+    htmlbuf[jj]=serbuf[bi];
+    jj++;
+    if (jj>=MYBUFSIZE+HEADFOOTSIZE) {
+      jj=hlen; // should not happen unless header is too big
+    }
+    bi++;
+    if (bi>=MYBUFSIZE) {
+      bi=0;
+    }
+  }
+  htmlbuf[jj]='\0'; // end string
+  strcat(htmlbuf,"</pre>"); // tack on footer
+  server.send(200, "text/html", htmlbuf);
+  //String lala="this is the bitter end";
+  //server.sendContent(lala);
   Serial.println("sent2");
 }
 
@@ -54,11 +85,15 @@ void setup(void){
   Serial.begin(9600);
   WiFi.begin(ssid, password);
   Serial.println("Hello");
-  strcpy(serbuf,"<head>\r\n<meta http-equiv=\"refresh\" content=\"5\">\r\n</head>");
-  strcat(serbuf,"<h1>Go Data</h1><pre>");
-  bo=strlen(serbuf);
-  Serial.print("\r\nBuf init to:");
-  Serial.println(bo);
+
+  // initialize web page buffer
+  strcpy(htmlbuf,"<head>\r\n<meta http-equiv=\"refresh\" content=\"1\">\r\n</head>");
+  strcat(htmlbuf,"<h1>Data</h1><pre>");
+  hlen=strlen(htmlbuf);
+
+  // set up ring buffer indices
+  rbfirst = 0; // initial ring buffer position. Room for header/footer at beginning
+  rblast = strlen(serbuf)-1; // last position with data.
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -91,17 +126,42 @@ void loop(void){
   server.handleClient();
   if (Serial.available() > 0) {
     // get incoming byte:
-    //strcat(serbuf,(char)Serial.read());
-    bo = strlen(serbuf); // how long
-    if (bo < MYBUFSIZE-6) { // room to add on
-      serbuf[bo]=Serial.read();
-      serbuf[bo+1]=0;
-    } else {
-      strcpy(serbuf,"<head>\r\n<meta http-equiv=\"refresh\" content=\"2\">\r\n</head>");
-      strcat(serbuf,"<h1>Data</h1><pre>");
-      //Serial.println("");
-      //Serial.print(bo);
-      //Serial.println("resetting buffer");
+    serbuf[rblast]=Serial.read();
+    bo=rblast; // one place before new rblast
+    rblast++;
+    if (rblast>=MYBUFSIZE) {
+      // wrap buffer
+      rblast=0;
     }
+    if (rblast==rbfirst) { // buffer is full
+      // delete a line to make room
+      // start at rbfirst, search ring buffer
+      // first '\n' will be new starting point (delete a line)
+      ii=rbfirst;
+      while ((serbuf[ii]!='\n')&&(ii!=bo)) {  // stop if find \n OR go thru whole buffer
+        ii++;
+        if (ii>=MYBUFSIZE) { // wrap index
+          ii=0; // start of buffer after header
+        }
+      }
+      rbfirst=ii+1; // new start: one past either the next \n or rblast
+      if (rbfirst>=MYBUFSIZE) { // wrap
+        rbfirst=0;
+      }
+    }
+
+
+    
+//    bo = strlen(serbuf); // how long
+//    if (bo < MYBUFSIZE-6) { // room to add on
+//      serbuf[bo]=Serial.read();
+//      serbuf[bo+1]=0;
+//    } else {
+//      strcpy(serbuf,"<head>\r\n<meta http-equiv=\"refresh\" content=\"2\">\r\n</head>");
+//      strcat(serbuf,"<h1>Data</h1><pre>");
+//      //Serial.println("");
+//      //Serial.print(bo);
+//      //Serial.println("resetting buffer");
+//    }
   }
 }
